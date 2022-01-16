5597d022abaf9b2aac057f54d174250578472cc5
shashir22 <shashirgowdas67@gmail.com>
2022-01-17 01:17:09 +0530

    Added:Calculate empoyees daily wage for month

--- emp.sh
+++ emp.sh
@@ -2,19 +2,25 @@
 
 isFullTime=1;
 isPartTime=2;
+totalSalary=0;
 empRatePerHrs=20;
-randomCheck=$((RANDOM%3));
-case $randomCheck in
-	$isFullTime)
-		empHrs=8
-		;;
-	$isPartTime)
-		empHrs=4
-		;;
-	*)
-		empHrs=0
-		;;
-esac
-
-salary=$(($empRatePerHrs*$empHrs));
-echo $salary;
+numWorkingDays=20;
+for (( day=1;day<=$numWorkingDays;day++ ))
+do
+	empCheck=$((RANDOM%3));
+	case $empCheck in
+		$isFullTime) 
+			empHrs=8
+			;;
+		$isPartTime)
+			empHrs=4
+			;;
+		*)
+			empHrs=0
+			;;
+	esac
+	salary=$(($empRatePerHrs*$empHrs));
+	totalSalary=$(($totalSalary+$salary));
+done
+echo $totalSalary
+echo $empCheck
