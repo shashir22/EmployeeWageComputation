8589ea392601dfa4f51e1e78583930d2f0bcbd5e
shashir22 <shashirgowdas67@gmail.com>
2022-01-17 01:03:34 +0530

    Added:Calculate employees daily wages

--- emp.sh
+++ emp.sh
@@ -4,7 +4,11 @@ isPresent=1;
 randomCheck=$((RANDOM%2));
 if [ $isPresent -eq $randomCheck ];
 then
-	echo "Employee is Present";
+	empRatePerHr=20;
+	empHrs=8;
+	salary=$(($empRatePerHr*$empHrs));
+	echo $salary;
 else
-	echo "The Employee is Abscent";
+	salary=0;
+	echo $salary;
 fi
