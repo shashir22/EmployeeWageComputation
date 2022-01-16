39406c71619fb20f3ad74f05540bda9f94c60604
shashir22 <shashirgowdas67@gmail.com>
2022-01-17 01:11:37 +0530

    Added:Employee wage compuation problem using case statement

--- emp.sh
+++ emp.sh
@@ -4,14 +4,17 @@ isFullTime=1;
 isPartTime=2;
 empRatePerHrs=20;
 randomCheck=$((RANDOM%3));
-if [ $isFullTime -eq $randomCheck ];
-then
-	empHrs=8;
-elif [ $isPartTime -eq $randomCheck ];
-then
-	empHrs=4;
-else
-	empHrs=0;
-fi
-salary=$(($empRatePerHrs*$empHrs))
+case $randomCheck in
+	$isFullTime)
+		empHrs=8
+		;;
+	$isPartTime)
+		empHrs=4
+		;;
+	*)
+		empHrs=0
+		;;
+esac
+
+salary=$(($empRatePerHrs*$empHrs));
 echo $salary;
