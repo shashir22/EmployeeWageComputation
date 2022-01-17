96a342bca96660900a63478fbe425bf3408509ba
shashir22 <shashirgowdas67@gmail.com>
2022-01-17 11:54:30 +0530

    Added:Calculating emp wage til either total working days or hours reached

--- emp.sh
+++ emp.sh
@@ -4,23 +4,28 @@ isFullTime=1;
 isPartTime=2;
 totalSalary=0;
 empRatePerHrs=20;
-numWorkingDays=20;
-for (( day=1;day<=$numWorkingDays;day++ ))
+maxWorkingHrs=100;
+maxWorkingDays=20;
+totalEmpHrs=0;
+totalWorkingDays=0;
+while [[ $totalEmpHrs -lt $maxWorkingHrs && $totalWorkingDays -lt $maxWorkingDays ]]
 do
+	((totalWorkingDays++))
 	empCheck=$((RANDOM%3));
 	case $empCheck in
 		$isFullTime) 
-			empHrs=8
-			;;
+				empHrs=8
+				;;
 		$isPartTime)
-			empHrs=4
-			;;
+				empHrs=4
+				;;
 		*)
 			empHrs=0
 			;;
 	esac
-	salary=$(($empRatePerHrs*$empHrs));
-	totalSalary=$(($totalSalary+$salary));
+	totalEmpHrs=$(($totalEmpHrs+$empHrs))
+
 done
+
+totalSalary=$(($totalEmpHrs*$empRatePerHrs));
 echo $totalSalary
-echo $empCheck
